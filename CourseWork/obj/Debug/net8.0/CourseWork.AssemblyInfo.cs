8cfcd6a7adedcccb83d8c68b67cbd6ab03052cfa
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-02-05 13:46:14 +0200

    Remove hardcoded Role names and Session keys

--- CourseWork/obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ CourseWork/obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06400149166f15e048dbd8bb1fd583fd090fb0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac8b8e77e306fb515196af73a03d6a7056aab8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
